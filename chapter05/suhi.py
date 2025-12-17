4f3820c0437c7fd56d1a8260bff22f17417c40dd
itc-n24009 <n24009@std.it-college.ac.jp>
2025-12-17 15:33:26 +0900

    chapter5 12/17 数占い

---
+++ chapter05/suhi.py
@@ -0,0 +1,27 @@
+#数占い(数秘術)
+
+year = str(input("生まれた年はいつですか >>"))
+month = str(input("生まれた月はいつですか >>"))
+day = str(input("生まれた日はいつですか >>"))
+
+number = 0
+
+#number = int(year[0]) + int(year[1]) + int(year[2]) + int(year[3])
+
+for i in year:
+    number += int(i)
+
+for i in month:
+    number += int(i)
+
+for i in day:
+    number += int(i)
+print(number)
+
+while number>=10:
+    tmp=str(number)
+    number = 0
+    for i in tmp:
+        number += int(i)
+
+print(f"あなたの運命数は{number}です")
