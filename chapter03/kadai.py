3f69cfdd908807e909a7ded8bf2d2acd6a084b66
itc-n24009 <n24009@std.it-college.ac.jp>
2025-12-17 17:00:48 +0900

    programing 課題2

---
+++ chapter03/kadai.py
@@ -0,0 +1,16 @@
+#挨拶課題版
+def aisatsu(namae, jikantai):
+    if jikantai == 1:
+        print(f"{namae}さん、おはようございます")
+    elif jikantai == 2:
+        print(f"{namae}さん、こんにちは")
+    elif jikantai == 3:
+        print(f"{namae}さん、こんばんは")
+    else:
+        print(f"{namae}さん、おやすみなさい")
+
+
+name = input("名前を入力して下さい >>")
+print("朝...1 昼...2 晩...3 寝る前...4")
+jikan = int(input("時間帯を入力してください >>"))
+aisatsu(name,jikan)
